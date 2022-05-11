a06fcf5affe8d991398935fb631d727a2f02e04e
Bruno Gomes <icestonebruno@gmail.com>
2022-05-11 11:49:49 -0300

    fix port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors()); // vai dizer qual fron pode acessar esse back
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Server started on port 3333');
 })
